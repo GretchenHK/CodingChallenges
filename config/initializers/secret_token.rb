8ef9204783cce462eac6f2e7979fe081e33dd81b
vagrant <g.halasikun@gmail.com>
2015-08-03 16:35:46 +0000

    fibonacci

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CodingChallenges::Application.config.secret_key_base = '80d1dead472bea9652925b1b65f41a42ca26f6f13ade1b7d520e9b4e499c4bcf64d16eab1a441bbdd542064da0dd1d6aa85a75b683106df8d8642ccdbf139e4f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CodingChallenges::Application.config.session_store :cookie_store, key: '_coding_challenges_session'
