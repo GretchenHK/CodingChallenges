1d04739650b74b8fd896aac684ca12a2aec93637
vagrant <g.halasikun@gmail.com>
2015-08-13 19:03:44 +0000

    Collatz solved

--- coll.rb
+++ coll.rb
@@ -1,27 +1,54 @@
-#This program will calculate the next number in the collatz sequence. 
 
-#I need a program that will calculate the longest collatz sequence.
-#I have tried to get this to loop by writing "while n>1" on line 7, but that doesn't seem to work.
-#once I get this to loop till it reaches 1, then next step is to get the program to count the length of the sequence
-#and return the "n" that generated the longest sequence.  I think this involves an array, but I have yet to figure out
-#how to run a method and have the result of the method be added to an array.  As far as counting the number of items in 
-#an array, I believe that can be done with "number_of_items = sequence.length ..... 
-
-
-def collatz(n)
-    
-        if n <= 1
-            return n
-        end
-        
-        if n % 2 == 0 
-            n = n / 2
-            return n
-        else
-            n = 3 * n + 1
-            puts n
-        end
+def collatz(input)
+    chain = []
+    chain << input
+    nd = next_digit(input)
+    while nd != 1
+    chain << nd
+    nd = next_digit(nd)
+end
+    chain << nd
+    return chain.count
 end
 
-puts collatz(1000)
+def next_digit(input)
+    if input < 1
+        return input
+    end
+        
+    if input % 2 == 0 
+        nd = input / 2
+        return nd
+    else
+        nd = 3 * input + 1
+    end
+end
+
+
+collatz = []
+1000.times do |n|
+    n = n + 1
+    collatz << collatz(n)
+end
+max_collatz = collatz.max
+puts collatz.index(max_collatz) + 1
+
+
+
+
+
+##collatz = []
+#1000.times do |n|
+#    n = n + 1
+#    collatz << collatz(n)
+#end
+#max = 0
+
+#if collatz(n) > max 
+
+
+#7.times do |n|
+#    n = n + 1
+ #   puts collatz(n)
+#end
 
